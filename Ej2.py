49ae77e95831d215aa02eaf55110cc607f39eb02
Brian Michelin <michelinbriandev@gmail.com>
2021-09-13 15:18:16 -0300

    2 corregido

--- Ej2.py
+++ Ej2.py
@@ -1,8 +1,12 @@
 a=int(input("Ingrese un numero: "))
 b=0
+c=a
+
+if a<0:
+    a=a*-1
 
 while (a>0):
     a=a//10
     b+=1
 
-print("El numero tiene",b,"digitos")
+print("El numero",c,"tiene",b,"digitos")
